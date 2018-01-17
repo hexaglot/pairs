1cbfdba174d98d901e4dbdfbdcd124f9f86b71ea
iles <tomiles@gmail.com>
2018-01-17 13:05:38 +0000

    refactored callback to remove from for loop logic

--- app.js
+++ app.js
@@ -1,69 +1,108 @@
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i -= 1) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-}
-
-//cards can be in two states - flpped not flipped
-//they can also be in two other states in-play or complete
-//user interacts in two ways - picking either first card, or second card
-
 $(document).ready(function () {
     //on load 
     const width = 4;
-    const cells = width * width;
     const images = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    let tiles = images.concat(images);
 
     const grid = $('#game-grid');
 
-    let first;
-    let first_cell;
-
     var init_game = function () {
 
-        //shuffle(tiles);
+        let state = {
+            // first : {card : undefined, value : undefined},
+            first: undefined,
+            tiles: images.concat(images),
+            move_tally: 0,
+            pairs_found: 0,
+            time: 0,
+            stars: 0
+        };
 
-        let move_tally = 0;
-        let pairs_found = 0;
-        let time = 0;
-        let stars = 0;
+        function shuffle(a) {
+            for (let i = a.length - 1; i > 0; i -= 1) {
+                const j = Math.floor(Math.random() * (i + 1));
+                const temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+
+        //shuffle(state.tiles);
+
 
         grid.find('*').remove();
-        $('#score').text(move_tally);
-        $('#timer').text(time);
+        $('#score').text(state.move_tally);
+        $('#timer').text(state.time);
 
         function render_stars(stars) {
             const star_hollow = '\u2606';
             const star_filled = '\u2605';
-            $('.stars').text(star_filled.repeat(stars) + star_hollow.repeat(3 - stars));
+            $('.stars').text(star_filled.repeat(state.stars) + star_hollow.repeat(3 - state.stars));
         }
 
         function update_stars() {
             //to get three stars you need a score of 16 - perfect
             //to get two stars you need to complete it in 24 moves,
             //anything else is one star
-            if (move_tally <= 16) {
-                stars = 3;
-            } else if (move_tally <= 24) {
-                stars = 2;
+            if (state.move_tally <= 16) {
+                state.stars = 3;
+            } else if (state.move_tally <= 24) {
+                state.stars = 2;
             } else {
-                stars = 1;
+                state.stars = 1;
             }
         }
 
         update_stars();
-        render_stars(stars);
+        render_stars(state.stars);
+
+        function card_click(e) {
+            const current = e.data;
+            //don't do anything if card already flipped
+            if (current.card.hasClass('flip')) {
+                return;
+            }
+
+            current.card.addClass('flip');
+            state.move_tally += 1;
+            $('#score').text(state.move_tally);
+            update_stars();
+            render_stars(state.stars);
+
+            if (state.first === undefined) {
+                //first choice
+                state.first = current;
+            } else if (state.first != undefined) {
+                //second choice
+                if (state.tiles[current.value] === state.tiles[state.first.value]) {
+                    //match found
+                    state.pairs_found += 1;
+                    //check if we have won
+                    if (state.pairs_found === 8) {
+                        $('#final-score').text(state.move_tally);
+                        //stop the timer
+                        clearInterval(timer_id);
+                        $('#final-time').text(state.time);
+                        $('.game-screen').hide();
+                        $('.win-screen').show();
+                    }
+                } else {
+                    //no match found
+                    setTimeout(function (f) {
+                        current.card.removeClass('flip');
+                        f.removeClass('flip');
+
+                    }, 1000, state.first.card);
+                }
+                state.first = undefined;
+            }
+        };
 
         let cell = 0;
         for (let row = 0; row < width; row += 1) {
             const tr = $('<tr></tr>');
 
             for (let col = 0; col < width; col += 1) {
-                const src = "img/" + tiles[cell] + ".svg";
+                const src = "img/" + state.tiles[cell] + ".svg";
                 const td = $('<td></td>');
 
                 const flipper_container = $('<div class="flip-container">\
@@ -74,68 +113,18 @@ $(document).ready(function () {
                     </div>');
 
                 td.append(flipper_container);
-
-                td.on('click', { cell: cell }, function (e) {
-
-                    const this_cell = e.data.cell;
-                    //don't do anything if card already flipped
-                    if (flipper_container.hasClass('flip')) {
-                        return;
-                    }
-
-                    flipper_container.addClass('flip');
-                    if (first === undefined) {
-                        //first choice
-                        first = flipper_container;
-                        first_cell = this_cell;
-                    } else if (first != undefined) {
-                        //second choice
-                        if (tiles[this_cell] === tiles[first_cell]) {
-                            //match found
-                            //                        first = undefined;
-                            //                      first_cell = undefined;
-                            pairs_found += 1;
-                            //check if we have won
-                            if (pairs_found === 8) {
-                                console.log("You win!");
-                                $('#final-score').text(move_tally);
-                                //stop the timer
-                                clearInterval(timer_id);
-                                $('#final-time').text(time);
-                                $('.game-screen').hide();
-                                $('.win-screen').show();
-                            }
-                        } else {
-                            //no match found
-                            setTimeout(function (f) {
-                                flipper_container.removeClass('flip');
-                                f.removeClass('flip');
-
-                            }, 1000, first);
-                        }
-                        first = undefined;
-                        first_cell = undefined;
-
-                    }
-                    move_tally += 1;
-                    $('#score').text(move_tally);
-                    update_stars();
-                    render_stars(stars);
-                });
-
+                td.on('click', { card: flipper_container, value: cell }, card_click);
                 tr.append(td);
                 cell += 1;
-
             }
             grid.append(tr);
         }
 
         const timer_id = window.setInterval(function () {
-            time += 1;
-            $('#timer').text(time);
+            state.time += 1;
+            $('#timer').text(state.time);
         }, 1000);
     };
-
 
     init_game();
 
